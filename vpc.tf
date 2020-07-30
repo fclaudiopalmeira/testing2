145556a3cb5bdd17fca7248561ed563d31812d13
Francisco Palmeira <FrancisP@cdhb.local>
2020-07-31 09:14:43 +1200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,6 +7,8 @@
 #
 
 ## Data Retrieval Block
+data "aws_vpcs" "observ-sec-eks" {}
+
 data "aws_vpc" "observ-sec-eks" {
  count = "${length(data.aws_vpcs.observ-sec-eks.ids)}"
  id    = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 0)
@@ -16,7 +18,7 @@ data "aws_subnet_ids" "eks_subnets" {
  vpc_id   = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 0)
 }
 
-data "aws_subnet" "eks_subnet" {
+data "aws_subnet" "eks_subnets" {
   for_each = data.aws_subnet_ids.eks_subnets.ids
   id    = each.value
 }
