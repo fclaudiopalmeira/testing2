17bb3606e56e95d9dc1ad86174630cc7bac77d90
Francisco Palmeira <FrancisP@cdhb.local>
2020-07-30 15:47:38 +1200

    Update eks-worker-nodes.tf

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -20,9 +20,13 @@ resource "aws_eks_node_group" "observ-sec-eks-node-group" {
 
   ]
 
-  tags =
+ /*  tags =
     {
       "k8s.io/cluster-autoscaler/enabled" = "true"
       "k8s.io/cluster-autoscaler/${var.cluster-name}" = "owned"
-    }
+    } */
+    tags = {
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
 }
