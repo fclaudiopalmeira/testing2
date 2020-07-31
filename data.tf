d1577abd5b45464bca57860ea186fdf3ca58bff9
Francisco Palmeira <FrancisP@cdhb.local>
2020-07-31 12:36:57 +1200

    Data.tf created
    Created the data.tf file

---
+++ data.tf
@@ -0,0 +1,21 @@
+## Data Retrieval Block
+data "aws_vpcs" "observ-sec-eks" {
+  filter {
+    name   = "tag:Name"
+    values = ["terraform-eks-observ-sec-eks-vpc"]
+  }
+}
+
+data "aws_vpc" "observ-sec-eks" {
+ count = "${length(data.aws_vpcs.observ-sec-eks.ids)}"
+ id    = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 0)
+}
+
+data "aws_subnet_ids" "eks_subnets" {
+ vpc_id   = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 0)
+}
+
+data "aws_subnet" "eks_subnets" {
+  for_each = data.aws_subnet_ids.eks_subnets.ids
+  id    = each.value
+}
--- vpc.tf
+++ vpc.tf
@@ -6,28 +6,6 @@
 #  * Route Table
 #
 
-## Data Retrieval Block
-data "aws_vpcs" "observ-sec-eks" {
-  filter {
-    name   = "tag:Name"
-    values = ["terraform-eks-observ-sec-eks-vpc"] # insert value here
-  }
-}
-
-data "aws_vpc" "observ-sec-eks" {
- count = "${length(data.aws_vpcs.observ-sec-eks.ids)}"
- id    = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 0)
-}
-
-data "aws_subnet_ids" "eks_subnets" {
- vpc_id   = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 0)
-}
-
-data "aws_subnet" "eks_subnets" {
-  for_each = data.aws_subnet_ids.eks_subnets.ids
-  id    = each.value
-}
-
 # Resources Blocks
 resource "aws_vpc" "observ-sec-eks" {
   cidr_block = "10.0.0.0/16"
