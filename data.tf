61eaaa1a88cfd6aa441cf92fdad0f27c730e913e
Francisco Palmeira <FrancisP@cdhb.local>
2020-07-31 12:46:06 +1200

    Update data.tf

--- data.tf
+++ data.tf
@@ -7,8 +7,12 @@ data "aws_vpcs" "observ-sec-eks" {
 }
 
 data "aws_vpc" "observ-sec-eks" {
- count = "${length(data.aws_vpcs.observ-sec-eks.ids)}"
- id    = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 1)
+ /* count = "${length(data.aws_vpcs.observ-sec-eks.ids)}"
+ id    = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 1) */
+   filter {
+    name   = "tag:Name"
+    values = ["terraform-eks-observ-sec-eks-vpc"]
+  }
 }
 
 data "aws_subnet_ids" "eks_subnets" {
