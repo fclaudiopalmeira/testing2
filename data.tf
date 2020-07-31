4f6b7ed659606d22358b6908977113f2c7abce1f
Francisco Palmeira <FrancisP@cdhb.local>
2020-07-31 14:08:20 +1200

    Dependencies added

--- data.tf
+++ data.tf
@@ -1,9 +1,12 @@
-/* ## Data Retrieval Block
+## Data Retrieval Block
 data "aws_vpcs" "observ-sec-eks" {
   filter {
     name   = "tag:Name"
     values = ["terraform-eks-observ-sec-eks-vpc"]
   }
+  depends_on = [
+    aws_vpc.observ-sec-eks,
+  ]
 }
 
 data "aws_vpc" "observ-sec-eks" {
@@ -19,4 +22,3 @@ data "aws_subnet" "eks_subnets" {
   for_each = data.aws_subnet_ids.eks_subnets.ids
   id    = each.value
 }
- */
--- vpc.tf
+++ vpc.tf
@@ -16,29 +16,6 @@ resource "aws_vpc" "observ-sec-eks" {
   )
 }
 
-## Data Retrieval Block
-data "aws_vpcs" "observ-sec-eks" {
-  filter {
-    name   = "tag:Name"
-    values = ["terraform-eks-observ-sec-eks-vpc"]
-  }
-}
-
-data "aws_vpc" "observ-sec-eks" {
- count = "${length(data.aws_vpcs.observ-sec-eks.ids)}"
- id    = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 0)
-}
-
-data "aws_subnet_ids" "eks_subnets" {
- vpc_id   = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 0)
-}
-
-data "aws_subnet" "eks_subnets" {
-  for_each = data.aws_subnet_ids.eks_subnets.ids
-  id    = each.value
-}
-
-
 resource "aws_subnet" "observ-sec-eks" {
   for_each = var.subnet_numbers
   availability_zone       = each.key
