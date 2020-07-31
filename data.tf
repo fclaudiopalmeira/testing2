0376fcadd4673dd255f69853c232953b5359fe2f
Francisco Palmeira <FrancisP@cdhb.local>
2020-07-31 12:41:56 +1200

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,14 +1,14 @@
 ## Data Retrieval Block
-/* data "aws_vpcs" "observ-sec-eks" {
+data "aws_vpcs" "observ-sec-eks" {
   filter {
     name   = "tag:Name"
     values = ["terraform-eks-observ-sec-eks-vpc"]
   }
 }
- */
+
 data "aws_vpc" "observ-sec-eks" {
  count = "${length(data.aws_vpcs.observ-sec-eks.ids)}"
- id    = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 0)
+ id    = element(tolist(data.aws_vpcs.observ-sec-eks.ids), 1)
 }
 
 data "aws_subnet_ids" "eks_subnets" {
